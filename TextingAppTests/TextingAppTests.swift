b5919e3a47d650866ab054ee8d4ca31d8c61be4e
stagiaire <stagiaire@iMac2.local>
2017-05-15 10:05:50 +0200

    Initial commit

---
+++ TextingAppTests/TextingAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  TextingAppTests.swift
+//  TextingAppTests
+//
+//  Created by stagiaire on 15/05/2017.
+//  Copyright © 2017 stagiaire. All rights reserved.
+//
+
+import XCTest
+@testable import TextingApp
+
+class TextingAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
