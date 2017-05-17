d9c78d57820f631a5b09af0fe2e9803de6b94ea4
stagiaire <stagiaire@iMac-de-admin.local>
2017-05-17 11:11:53 +0200

    commit mylene mercredi

--- TextingApp/CompteTableViewController.swift
+++ TextingApp/CompteTableViewController.swift
@@ -8,9 +8,80 @@
 
 import UIKit
 
+struct cellData {
+    let cell : Int!
+    let button : UIButton!
+    let image : UIImage!
+    }
+
+
+
 class CompteTableViewController: UITableViewController {
+
+    var arrayOfCellData = [cellData]()
     
-    let NomTable = ["Modification mot de passe", "Supression de compte", "Déconnexion"]
+    override func viewDidLoad(){
+        
+        arrayOfCellData = [cellData(cell : 1, button : "Modification du mots de passe", image : #imageLiteral(resourceName: "cadenas")),
+                           cellData(cell : 2, button : "Suppression du compte", image : #imageLiteral(resourceName: "pres-effacer-supprimer-l-39-icone-carre_318-9256")),
+                           cellData(cell: 3, button : "Déconnexion", image : #imageLiteral(resourceName: "deconnexion_318-10026"))
+        ]
+    
+    }
+    override func tableView(_ tableView : UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return arrayOfCellData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        if arrayOfCellData[indexPath.row].cell == 1{
+            let cell = Bundle.main.loadNibNamed("TableViewCell1", owner : self, options: nil)?.first as! TableViewCell1
+            cell.mainImageView.image = arrayOfCellData[indexPath.row].image
+            cell.mainLbl.titleLabel = arrayOfCellData[indexPath.row].titleLabel
+            
+            return cell
+        }
+        else if arrayOfCellData[indexPath.row].cell == 2 {
+            let cell = Bundle.main.loadNibNamed("TableViewCell2", owner : self, options: nil)?.first as! TableViewCell2
+            cell.mainImageView2.image = arrayOfCellData[indexPath.row].image
+            cell.mainLbl2.button = arrayOfCellData[indexPath.row].button
+            
+            return cell
+        }
+        else if arrayOfCellData[indexPath.row].cell == 3 {
+            let cell = Bundle.main.loadNibNamed("TableViewCell3", owner : self, options: nil)?.first as! TableViewCell3
+            cell.mainImageView3.image = arrayOfCellData[indexPath.row].image
+            cell.mainLbl3.button = arrayOfCellData[indexPath.row].button
+            return cell
+        }
+            
+        else {
+            let cell = Bundle.main.loadNibNamed("TableViewCell1", owner : self, options: nil)?.first as! TableViewCell1
+            cell.mainImageView.image = arrayOfCellData[indexPath.row].image
+            cell.mainLbl.button = arrayOfCellData[indexPath.row].button
+            
+            return cell
+        }
+    
+    }
+    override func tableView(_ table_View: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+    
+    if arrayOfCellData[indexPath.row].cell == 1{
+        return 100
+    }
+    else if arrayOfCellData[indexPath.row].cell == 2 {
+        return 100
+    }
+    else if arrayOfCellData[indexPath.row].cell == 3 {
+        return 100
+    }
+    
+    else {
+        return 100
+    
+    }
+
     
     
     
@@ -22,43 +93,46 @@ class CompteTableViewController: UITableViewController {
     
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    //let NomTable = ["Modification mot de passe", "Supression de compte", "Déconnexion"]
+    
+    //override func viewDidLoad() {
+      //  super.viewDidLoad()
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
+    //}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    //override func didReceiveMemoryWarning() {
+//super.didReceiveMemoryWarning()
+     //   // Dispose of any resources that can be recreated.
+    //}
 
     // MARK: - Table view data source
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    //override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
+    //    return 1
+    //}
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return self.NomTable.count
-    }
+    //    return self.NomTable.count
+   // }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
+    //override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     //   let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
 
         // Configure the cell...
 
-        cell.textLabel?.text = self.NomTable[indexPath.row]
+      //  cell.textLabel?.text = self.NomTable[indexPath.row]
         
-        return cell
-    }
+       // return cell
+    //}
     
     
     
@@ -116,4 +190,5 @@ class CompteTableViewController: UITableViewController {
     }
     */
 
+}
 }
--- TextingApp/TableViewCell1.swift
+++ TextingApp/TableViewCell1.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class TableViewCell1: UITableViewCell {
+internal class TableViewCell1: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+
+    @IBOutlet weak var mainLbl: UIButton!
+    
+    //@IBOutlet weak var mainLbl: UILabel!
+  
+    
+    
+    //override func awakeFromNib() {
+        //super.awakeFromNib()
         // Initialization code
-    }
+   // }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    //override func setSelected(_ selected: Bool, animated: Bool) {
+      //  super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
-    }
+   // }
     
 }
