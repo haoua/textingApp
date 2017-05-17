0918f546f0f585c63d53a0bd9e3719b505216940
Aymeric <aymeric.lu@gmail.com>
2017-05-17 16:18:53 +0200

    ajout du menu conversation

---
+++ TextingApp/ContactViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ContactViewController.swift
+//  TextingApp
+//
+//  Created by stagiaire on 17/05/2017.
+//  Copyright © 2017 stagiaire. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let textCellIdentifier = "TextCell"
+    
+    let swiftBlogs = ["AZAZ4545", "DSDS8787", "SDSDSD878"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftBlogs.count
+    }
+    
+    func tableView(_ cellForRowAttableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath as IndexPath)
+        
+        let row = indexPath.row
+        cell.textLabel?.text = swiftBlogs[row]
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
