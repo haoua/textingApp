3016a20dc7767a1597c75da5af127e2208c9a314
stagiaire <stagiaire@iMac1.local>
2017-05-18 14:56:13 +0200

    update : create login, auth and search contact

--- TextingApp/SecondViewController.swift
+++ TextingApp/SecondViewController.swift
@@ -7,22 +7,92 @@
 //
 
 import UIKit
+import Alamofire
 
 class SecondViewController: UIViewController {
 
-    @IBOutlet weak var identifiantUnique: UILabel!
+    var configUser:ConfigUser = ConfigUser.shared
+   
+    let defaults = UserDefaults.standard
+    
+    
+    
+    @IBOutlet weak var password: UITextField!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let uuid = UUID().uuidString
+        //Server gives a uid (uuid)
+        //because server check if uid is unique in local DB
+        /*let uuid = UUID().uuidString
+        self.identifiantUnique.text = uuid*/
         
-        self.identifiantUnique.text = uuid
+        
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func createBtn() {
+        print(self.password.text ?? " ")
+        
+        if((self.password.text) != nil){
+            
+            let parameters: Parameters = ["password": self.password.text ?? " "]
+            
+            Alamofire.request(CommunUrlApi.Global.urlPostUsersCreate, method: .post, parameters: parameters).responseJSON { response in
+                
+                //print(response.request ?? "")  // original URL request
+                //print(response.response ?? "") // HTTP URL response
+                //print(response.data ?? "")     // server data
+                //print(response.result)   // result of response serialization
+                
+                print(response)
+                
+                if let result = response.result.value {
+                    let JSON = result as! NSDictionary
+                    print(JSON.object(forKey: "uid") ?? "")
+                }
+                
+                if let status = response.response?.statusCode {
+                    
+                    print(status)
+                    
+                    switch(status){
+                    case 200:
+                        print("example success")
+                        
+                        if let result = response.result.value {
+                            let JSON = result as! NSDictionary
+                            print(JSON.object(forKey: "uid") ?? "")
+                            
+                            self.configUser.uid = JSON.object(forKey: "uid") as! String
+                            let uid = JSON.object(forKey: "uid") as! String
+                            
+                            // Store
+                            self.defaults.set(uid, forKey: "uid")
+                            self.defaults.synchronize()
+                   
+                            self.dismiss(animated: true, completion: nil)
+                            
+                        }
+                        
+                    default:
+                        print("error with response status: \(status)")
+                    }
+                }
+                
+            }
+            
+        }else{
+            print("Vous devez taper un mot de passe !");
+            
+        }
+                
     }
     
 
--- TextingApp/ThreeViewController.swift
+++ TextingApp/ThreeViewController.swift
@@ -7,9 +7,44 @@
 //
 
 import UIKit
+import Alamofire
+import Foundation
+
 
 class ThreeViewController: UIViewController {
 
+    let defaults = UserDefaults.standard
+    var token:String = ""
+    var myUid = "";
+    
+    
+    @IBOutlet weak var inputSearchUid: UITextField!
+    @IBOutlet weak var msgTextInfo: UITextField!
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated) // No need for semicolon
+        
+        self.inputSearchUid.text = "718ece65-ab5b-45f2-aa2c-3265e3beebc3"
+        
+        if let token = defaults.string(forKey: "token"){
+            print(token)
+            self.token = token
+        }else{
+            self.token = ""
+        }
+        
+        if let uid = defaults.string(forKey: "uid"){
+            print(uid)
+            self.myUid = uid
+            
+        }else{
+            self.myUid = ""
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +57,59 @@ class ThreeViewController: UIViewController {
     }
     
 
+    @IBAction func searchBtn(_ sender: Any) {
+        
+        print(CommunUrlApi.Global.urlPostFindContact)
+        
+        
+        if(self.token != "" && self.inputSearchUid.text != nil && (self.inputSearchUid.text != self.myUid) ){
+            
+            let uid = self.inputSearchUid.text
+            let headers: HTTPHeaders = ["x-access-token": self.token]
+            let parameters: Parameters = ["uid": uid!]
+            
+            Alamofire.request("http://192.168.1.138:3000/contacts/find/contact", method: .post, parameters: parameters, headers: headers).responseJSON { response in
+                
+                print(response.request ?? "")  // original URL request
+                print(response.response ?? "") // HTTP URL response
+                print(response.data ?? "")     // server data
+                print(response.result)   // result of response serialization
+                
+                print(response)
+                
+                if let result = response.result.value {
+                    let JSON = result as! NSDictionary
+                    
+                    //let uid:String = (JSON.object(forKey: "uid") as! String)
+                    let msg:String = JSON.object(forKey: "msg") as! String
+                    self.msgTextInfo.text = msg
+                    
+                    if let responseStatus = response.response?.statusCode {
+                        if responseStatus != 200 {
+                            // error
+                            
+                            
+                        } else {
+                            // Afficher le form pour ajouter le contact
+                            
+                        }
+                    }
+                    
+                    
+                }else{
+                    
+                }
+            }
+            
+        }else{
+            self.msgTextInfo.text = "Vous devez taper un uid !"
+            
+        }
+        
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
