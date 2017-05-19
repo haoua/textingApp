bf47e764e0120f2017477dc481dc7cc4ca11b3eb
Aymeric <aymeric.lu@gmail.com>
2017-05-19 14:24:42 +0200

    Adding change of mambot branch (merge is breaking everything)

--- TextingApp/ThreeViewController.swift
+++ TextingApp/ThreeViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
-
+import Alamofire
+import Foundation
 import Security
 
 class ThreeViewController: UIViewController {
     
-    @IBOutlet weak var textidentifiant: UITextField!
+    let defaults = UserDefaults.standard
+    var token:String = ""
+    var myUid = "";
     
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var inputSearchUid: UITextField!
+  
+    @IBOutlet weak var msgTextInfo: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated) // No need for semicolon
+        
+        self.inputSearchUid.text = "718ece65-ab5b-45f2-aa2c-3265e3beebc3"
+        
+        if let token = defaults.string(forKey: "token"){
+            print(token)
+            self.token = token
+        }else{
+            self.token = ""
+        }
+        
+        if let uid = defaults.string(forKey: "uid"){
+            print(uid)
+            self.myUid = uid
+            
+        }else{
+            self.myUid = ""
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +56,57 @@ class ThreeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func searchBtn(_ sender: Any) {
+        
+        print(CommunUrlApi.Global.urlPostFindContact)
+        
+        
+        if(self.token != "" && self.inputSearchUid.text != nil && (self.inputSearchUid.text != self.myUid) ){
+            
+            let uid = self.inputSearchUid.text
+            let headers: HTTPHeaders = ["x-access-token": self.token]
+            let parameters: Parameters = ["uid": uid!]
+            
+            Alamofire.request("http://192.168.1.138:3000/contacts/find/contact", method: .post, parameters: parameters, headers: headers).responseJSON { response in
+                
+                print(response.request ?? "")  // original URL request
+                print(response.response ?? "") // HTTP URL response
+                print(response.data ?? "")     // server data
+                print(response.result)   // result of response serialization
+                
+                print(response)
+                
+                if let result = response.result.value {
+                    let JSON = result as! NSDictionary
+                    
+                    //let uid:String = (JSON.object(forKey: "uid") as! String)
+                    let msg:String = JSON.object(forKey: "msg") as! String
+                    self.msgTextInfo.text = msg
+                    
+                    if let responseStatus = response.response?.statusCode {
+                        if responseStatus != 200 {
+                            // error
+                            
+                            
+                        } else {
+                            // Afficher le form pour ajouter le contact
+                            
+                        }
+                    }
+                    
+                    
+                }else{
+                    
+                }
+            }
+            
+        }else{
+            self.msgTextInfo.text = "Vous devez taper un uid !"
+            
+        }
+        
+    }
+    
     func configureCustomTextField() {
         
         let txtLoginImage = UIImage(named: "search")!
@@ -37,56 +115,56 @@ class ThreeViewController: UIViewController {
         let txtLoginImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
         txtLoginImageView.image = txtLoginImage
         
-        // 3. Ajout de notre conteneur d'image à gauche de la zone de texte
-        textidentifiant.leftView = txtLoginImageView
-        textidentifiant.leftViewMode = .always
-        
-        // 4. Paramètrage de la zone
-        textidentifiant.placeholder = NSLocalizedString("Identifiant", comment: "")  // texte affiché tant qu'aucune saisie n'est effectuée
-        textidentifiant.autocorrectionType = .no   // Pas de correction automatique
-        textidentifiant.returnKeyType = .done      // Paramètrage de la touche "Retour" sur Done
+//        // 3. Ajout de notre conteneur d'image à gauche de la zone de texte
+        inputSearchUid.leftView = txtLoginImageView
+        inputSearchUid.leftViewMode = .always
+//        
+//        // 4. Paramètrage de la zone
+        inputSearchUid.placeholder = NSLocalizedString("Identifiant", comment: "")  // texte affiché tant qu'aucune saisie n'est effectuée
+       inputSearchUid.autocorrectionType = .no   // Pas de correction automatique
+       inputSearchUid.returnKeyType = .done      // Paramètrage de la touche "Retour" sur Done
     }
     
 
-    @IBOutlet weak var btncrypto: UIButton!
-    
-    @IBAction func btnActioncrypto(_ sender: UIButton) {
-        let parameters: [String: AnyObject] = [
-            kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
-            kSecAttrKeySizeInBits as String: 1024 as AnyObject]
-        
-        var publicKey, privateKey: SecKey?
-        
-        SecKeyGeneratePair(parameters as CFDictionary, &publicKey, &privateKey)
-        
-        //Encrypt a string with the public key
-        let message = textidentifiant.text
-        let blockSize = SecKeyGetBlockSize(publicKey!)
-        var messageEncrypted = [UInt8](repeating: 0, count: blockSize)
-        var messageEncryptedSize = blockSize
-        
-        var status: OSStatus!
-        
-        status = SecKeyEncrypt(publicKey!, SecPadding.PKCS1, message!, (message?.characters.count)!, &messageEncrypted, &messageEncryptedSize)
-        
-        if status != noErr {
-            print("Encryption Error!")
-            return
-        }
-        
-        //Decrypt the entrypted string with the private key
-        var messageDecrypted = [UInt8](repeating: 0, count: blockSize)
-        var messageDecryptedSize = messageEncryptedSize
-        
-        status = SecKeyDecrypt(privateKey!, SecPadding.PKCS1, &messageEncrypted, messageEncryptedSize, &messageDecrypted, &messageDecryptedSize)
-        
-        if status != noErr {
-            print("Decryption Error!")
-            return
-        }
-        
-        print(NSString(bytes: &messageDecrypted, length: messageDecryptedSize, encoding: String.Encoding.utf8.rawValue)!)
-    }
+//    @IBOutlet weak var btncrypto: UIButton!
+//    
+//    @IBAction func btnActioncrypto(_ sender: UIButton) {
+//        let parameters: [String: AnyObject] = [
+//            kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+//            kSecAttrKeySizeInBits as String: 1024 as AnyObject]
+//        
+//        var publicKey, privateKey: SecKey?
+//        
+//        SecKeyGeneratePair(parameters as CFDictionary, &publicKey, &privateKey)
+//        
+//        //Encrypt a string with the public key
+//        let message = textidentifiant.text
+//        let blockSize = SecKeyGetBlockSize(publicKey!)
+//        var messageEncrypted = [UInt8](repeating: 0, count: blockSize)
+//        var messageEncryptedSize = blockSize
+//        
+//        var status: OSStatus!
+//        
+//        status = SecKeyEncrypt(publicKey!, SecPadding.PKCS1, message!, (message?.characters.count)!, &messageEncrypted, &messageEncryptedSize)
+//        
+//        if status != noErr {
+//            print("Encryption Error!")
+//            return
+//        }
+//        
+//        //Decrypt the entrypted string with the private key
+//        var messageDecrypted = [UInt8](repeating: 0, count: blockSize)
+//        var messageDecryptedSize = messageEncryptedSize
+//        
+//        status = SecKeyDecrypt(privateKey!, SecPadding.PKCS1, &messageEncrypted, messageEncryptedSize, &messageDecrypted, &messageDecryptedSize)
+//        
+//        if status != noErr {
+//            print("Decryption Error!")
+//            return
+//        }
+//        
+//        print(NSString(bytes: &messageDecrypted, length: messageDecryptedSize, encoding: String.Encoding.utf8.rawValue)!)
+//    }
 
     
     
